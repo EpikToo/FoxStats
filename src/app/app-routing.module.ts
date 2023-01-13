6a1552b7fdab86a0c327e841c141e686228334da
Florian Savalle <florian.savalle@covage.com>
2023-01-13 15:49:59 +0100

    Premières briques du front (routage, components principaux...)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: '', component: IndexComponent },
+    { path: 'map', component: MapComponent },
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    NavbarComponent,
+    FooterComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
